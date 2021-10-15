283d811199c0e56a21c8cee44c32e96b7bb94b45
craigbowden76 <cbowden@bowdoin.edu>
2021-10-15 10:34:16 -0400

    Update get_devices.py

--- get_devices.py
+++ get_devices.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import json
 
 base_url = 'https://netbox.bowdoin.edu/api/'
-uri = 'ipam/ip-addresses'
+uri = 'ipam/ip-addresses/?limit=0'
 url = base_url + uri
 token = getpass()
 # params = { 'name': "junos-dev-ex4200" }
@@ -17,5 +17,12 @@ r = requests.get(url, headers=headers)
 output = json.loads(r.text)
 devices = output['results']
 
+addresses = []
+
 for each in devices:
-    pprint(each['address'])
+    cidr = each['address']
+    ipv4_addr = (cidr.split("/", 1)[0])
+    addresses.append(ipv4_addr)
+
+for address in addresses:
+    print(address)
