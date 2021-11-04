01ec13393d27a9d0b520eb74a61ffc815739e79d
craigbowden76 <cbowden@bowdoin.edu>
2021-11-04 12:53:20 -0400

    device names

--- get_all_device_names.py
+++ get_all_device_names.py
@@ -5,8 +5,8 @@ import json
 
 # https://netbox.bowdoin.edu/api/docs/
 
-base_url = 'https://netbox.bowdoin.edu/api/'
-uri = 'ipam/ip-addresses/?limit=0'
+base_url = 'https://netbox.bowdoin.edu/'
+uri = 'api/dcim/devices/?limit=0'
 url = base_url + uri
 token = getpass()
 # params = { 'name': "junos-dev-ex4200" }
@@ -19,12 +19,15 @@ r = requests.get(url, headers=headers)
 output = json.loads(r.text)
 devices = output['results']
 
-addresses = []
-
+devices_names = []
 for each in devices:
-    cidr = each['address']
-    ipv4_addr = (cidr.split("/", 1)[0])
-    addresses.append(ipv4_addr)
+    device = each['name']
+    names = (device.split(":"))
+    devices_names.append(names[0])
+    #print(names[0])
 
-for address in addresses:
-    print(address)
+device_name = (list(set(devices_names)))
+device_name.sort()
+
+for those in device_name:
+    print(those)
